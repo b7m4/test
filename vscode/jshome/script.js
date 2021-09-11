d8bf24bdf48cb24c7e2fc17a6296fb4ca2557b84
b7m4 <89705287+b7m4@users.noreply.github.com>
2021-09-11 21:34:34 +0300

    test
    test

---
+++ vscode/jshome/script.js
@@ -0,0 +1,35 @@
+
+
+// ! дабавляет класс
+$('.wrapper').addClass('loaded');
+// ! дабавляет класс 
+$('.icon-menu').click(function (event) {
+    $(this).toggleClass('active');
+    $('.menu__body').toggleClass('active');
+    $('body').toggleClass('lock');
+});
+
+
+
+// ! ibg js код
+function ibg() {
+
+    let ibg = document.querySelectorAll(".ibg");
+    for (var i = 0; i < ibg.length; i++) {
+        if (ibg[i].querySelector('img')) {
+            ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+        }
+    }
+}
+
+ibg();
+// !это jquery ibg код
+//function ibg() {
+ //   $.each($('.ibg'), function (index, val) {
+ //       if($(this).find('img').length > 0) {
+  //          $(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+  //      }
+ //  });
+//}
+
+//ibg();
